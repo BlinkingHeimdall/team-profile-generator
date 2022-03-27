e3f386d98b55d564da810656d0943eea5ed6e7aa
Jason Adams <jmadams41@gmail.com>
2022-03-27 17:19:10 -0600

    Intern passes all tests

---
+++ _tests_/Intern.test.js
@@ -0,0 +1,10 @@
+
+const { expect } = require('@jest/globals');
+const Intern = require('../lib/Intern');
+
+test('This makes and intern object', () => {
+    const intern = new Intern('Maxwell', 'Utah', '12');
+    expect(intern.school()).toEqual(intern.internSchool);
+    expect(intern.role()).toBe(Intern);
+    expect(intern.getId()).toEqual(intern.internId);
+})
--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -2,7 +2,7 @@
 const Manager = require('../lib/Manager');
 
 test('makes a Manager object', () => {
-    const manager = new Manager('Maxine', '3', '1');
+    const manager = new Manager('Maxine', '1', '1');
     expect(manager.officeNumber()).toEqual(manager.managerOffice);
     expect(manager.getId()).toEqual(manager.managerId);
     expect(manager.role()).toBe(Manager);
