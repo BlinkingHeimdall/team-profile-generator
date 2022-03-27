8876b4545f59a665ee850e1d71e7a779a394d376
Jason Adams <jmadams41@gmail.com>
2022-03-27 17:23:16 -0600

    fixed issue with manager

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,7 +1,7 @@
 
 const Manager = require('../lib/Manager');
 
-test('makes a Manager object', () => {
+test('This makes a Manager object', () => {
     const manager = new Manager('Maxine', '1', '1');
     expect(manager.officeNumber()).toEqual(manager.managerOffice);
     expect(manager.getId()).toEqual(manager.managerId);
