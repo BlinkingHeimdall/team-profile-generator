7556e95e3a63632dcd6275e4ae971f4e4b9c464c
Jason Adams <jmadams41@gmail.com>
2022-03-27 13:50:38 -0600

    Employee passes all tests

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -2,4 +2,6 @@ const Employee = require('../lib/Employee')
 
 test('creates an Employee object',() => {
     const employee = new Employee('Max');
+    expect(employee.employeeName).toBe('Max');
+    expect(employee.getEmail()).toEqual(employee.employeeEmail);
 })
